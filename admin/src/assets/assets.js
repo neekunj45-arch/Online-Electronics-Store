645f3f447405b51bba39734708d886a951a7796d
neekunj45-arch <neekunj45@gmail.com>
2026-04-23 19:14:23 +0530

    admin

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,9 @@
+import image_upload from './upload.png'
+import bag from './bag.png'
+
+export const assets = {
+
+    image_upload,
+    bag
+
+}
---
+++ admin/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Navbar.css";
+const Navbar = () => {
+  
+
+  return (
+    <div>
+      <div className="navbar">
+        <h2>PNP Sales</h2>    
+          <span>👤</span>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
