951fb5db821d63e6295ecd527431aebbf4ef9e75
neekunj45-arch <neekunj45@gmail.com>
2026-04-24 09:58:33 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,39 @@
-import express from 'express'
-import cors from 'cors'
-import { connectDB } from './config/db.js'
-import 'dotenv/config'
-import productRouter from './routes/productRoute.js'
-import userRouter from './routes/userRoute.js'
-import cartRouter from './routes/cartRoute.js'
-import orderRouter from './routes/orderRoute.js'
-import adminRoutes from "./routes/adminRoute.js"
-
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import "dotenv/config";
+import productRouter from "./routes/productRoute.js";
+import userRouter from "./routes/userRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+import adminRoutes from "./routes/adminRoute.js";
 
 // app config
 
-const app = express()
-const port = 4000
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 // middleware
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-// Database connection 
+// Database connection
 connectDB();
 
 // api endpoints
-app.use("/api/product",productRouter)
-app.use("/images",express.static('uploads'))
-app.use("/api/user",userRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/order",orderRouter)
+app.use("/api/product", productRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 app.use("/api/admin", adminRoutes);
 
-app.get("/",(req,res)=>{
-    res.send("API Working")
-})
-
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
 
 // for run express server
-app.listen(port,()=>{
-    console.log(`Server Stated on http://localhost:${port}`)
-})
-
-// mongodb+srv://pnp_db_user:24785693@cluster0.ekczd0k.mongodb.net/?
-
-// optional mongodb+srv://pnp_db_user:24785693@cluster0.ekczd0k.mongodb.net/?appName=Cluster0
+app.listen(PORT, () => {
+  console.log(`Server Stated on http://localhost:${PORT}`);
+});
