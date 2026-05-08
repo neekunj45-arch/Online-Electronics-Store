9e468f111cabd73505cb239ffb864a98646916b8
neekunj45-arch <neekunj45@gmail.com>
2026-05-08 17:51:10 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems,setCartItems] = useState({});
-    const url = "http://localhost:4000"
+    const url = "https://online-electronics-store-backend.onrender.com"
     const [token,setToken] = useState("");
     const [product_list,setProductList] = useState([])
 
