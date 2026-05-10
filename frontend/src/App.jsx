8b6d8bdfaaf100a3384a916ec7b23f2c50c3cd88
neekunj45-arch <neekunj45@gmail.com>
2026-05-10 18:44:46 +0530

    update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
-import Cart from './pages/ShoppingCart/ShoppingCart'
+import ShoppingCart from './pages/ShoppingCart/ShoppingCart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
@@ -20,7 +20,7 @@ const App = () => {
       <Navbar setShowLogin={setShowLogin}/>
       <Routes>
         <Route path='/' element={ <Home/> } />
-          <Route path='/cart' element={ <Cart/> } />
+          <Route path='/cart' element={ <ShoppingCart/> } />
           <Route path='/order' element={<PlaceOrder/>} />
           <Route path='/verify' element={<Verify/>} />
           <Route path='/myorders' element={<MyOrders/>} />
